08f3c393b08ffe8cd4b1cdbc86fdf3fb2a5d3d84
Siddhant Vij <siddhantvij201@gmail.com>
2022-09-23 14:34:34 +0530

    Basics - Remove duplicate strings in an array

---
+++ src/myPackage/j1basics/RemoveDuplicateStrings.java
@@ -0,0 +1,40 @@
+package myPackage.j1basics;
+
+import java.util.Scanner;
+
+public class RemoveDuplicateStrings {
+  public static void main(String[] args) {
+
+    try (Scanner sc = new Scanner(System.in)) {
+      int n = sc.nextInt();
+      String[] strings = new String[n];
+      for (int i = 0; i < n; i++) {
+        strings[i] = sc.next();
+      }
+
+      for (int i = 0; i < n; i++) {
+        int count = 0;
+        if (strings[i] != null) {
+          for (int j = 0; j < n; j++) {
+            if (strings[j] != null) {
+              if (i != j) {
+                if (strings[i].equals(strings[j])) {
+                  count++;
+                  strings[j] = null;
+                }
+              }
+            }
+          }
+          if (count != 0)
+            strings[i] = null;
+        }
+      }
+      for (int i = 0; i < strings.length; i++) {
+        if (i == strings.length-1)
+          System.out.println(strings[i]);
+        else
+          System.out.print(strings[i] + ", ");
+      }
+    }
+  }
+}
