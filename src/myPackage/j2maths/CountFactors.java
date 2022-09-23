4261e7fb6559d713458195e069baf6ffed2c8bf8
Siddhant Vij <siddhantvij201@gmail.com>
2022-09-23 12:01:00 +0530

    Maths - Count Factors

---
+++ src/myPackage/j2maths/CountFactors.java
@@ -0,0 +1,28 @@
+package myPackage.j2maths;
+
+import java.util.Scanner;
+
+public class CountFactors {
+
+  static int solve (int A){
+    int count = 0;
+    for (int i = 1; i*i <= A; i++) {
+      if (A%i == 0)
+      {
+        if (i == A/i)
+          count++;
+        else
+          count+=2;
+      }
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      int x = sc.nextInt();
+      System.out.println(solve(x));
+    }
+  }
+  
+}
