1c65615192040b3d83bc065f506d1db1edc38145
Siddhant Vij <siddhantvij201@gmail.com>
2022-09-28 11:30:28 +0530

    Array Practice
    Integer to Array Form - Carry Forward Sum: LC 989

---
+++ src/myPackage/j5Arrays/ArrayFormInteger.java
@@ -0,0 +1,35 @@
+// Leetcode 989
+
+package myPackage.j5Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArrayFormInteger {
+
+  static List<Integer> addToArrayForm(int[] num, int k) {
+    List<Integer> arrL = new ArrayList<>();
+    for (int i = num.length - 1; i >= 0; i--) {
+      arrL.add(0, (num[i] + k) % 10);
+      k = (num[i] + k) / 10;
+    }
+    while (k > 0) {
+      arrL.add(0, k % 10);
+      k /= 10;
+    }
+    return arrL;
+  }
+
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      int n = sc.nextInt();
+      int[] arr = new int[n];
+      for (int i = 0; i < n; i++) {
+        arr[i] = sc.nextInt();
+      }
+      int k = sc.nextInt();
+      System.out.println(addToArrayForm(arr, k));
+    }
+  }
+}
