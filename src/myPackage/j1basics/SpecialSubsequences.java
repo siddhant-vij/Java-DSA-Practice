2f5ef301b623ac27f92868a6f53e74e504058dff
Siddhant Vij <siddhantvij201@gmail.com>
2022-10-07 11:01:20 +0530

    Special Subsequences "AG"

---
+++ src/myPackage/j1basics/SpecialSubsequences.java
@@ -0,0 +1,25 @@
+package myPackage.j1basics;
+
+import java.util.Scanner;
+
+public class SpecialSubsequences {
+
+  static int countSubsequences(String str) {
+    int count = 0;
+    int countg = 0;
+    for (int i = str.length() - 1; i >= 0; i--) {
+      if (str.charAt(i) == 'G')
+        countg++;
+      if (str.charAt(i) == 'A')
+        count = count % 1000000007 + countg % 1000000007;
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      String str = sc.next();
+      System.out.println(countSubsequences(str));
+    }
+  }
+}
