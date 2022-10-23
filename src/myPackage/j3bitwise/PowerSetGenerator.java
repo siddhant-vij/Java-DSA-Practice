fcc4806f27fcd06e96f9f326140604e263f02641
Siddhant Vij <siddhantvij201@gmail.com>
2022-10-23 17:39:50 +0530

    Power Set Generator
    1. Leetcode: 78 - Subsets
    2. Leetcode: 90 - Subsets II

--- src/myPackage/j3bitwise/PowerSetGenerator.java
+++ src/myPackage/j3bitwise/PowerSetGenerator.java
@@ -1,20 +1,66 @@
+// Power Set Generator
+// Leetcode: 78 - Subsets
+// Leetcode: 90 - Subsets II
+
 package myPackage.j3bitwise;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class PowerSetGenerator {
+
+  static List<List<Integer>> subsetsWithDups(int[] nums) {
+    Arrays.sort(nums);
+    int n = nums.length;
+    List<List<Integer>> results = new ArrayList<>();
+    int max = (int) Math.pow(2, n);
+    for (int i = 0; i < max; i++) {
+      List<Integer> result = new ArrayList<>();
+      for (int j = 0; j < n; j++) {
+        if ((i & (1 << j)) != 0)
+          result.add(nums[j]);
+      }
+      if (results.size() == 0)
+        results.add(result);
+      else {
+        int count = 0;
+        for (int k = 0; k < results.size(); k++) {
+          if (result.equals(results.get(k)))
+            count++;
+        }
+        if (count == 0)
+          results.add(result);
+      }
+    }
+    return results;
+  }
+
+  static List<List<Integer>> subsets(int[] nums) {
+    int n = nums.length;
+    List<List<Integer>> results = new ArrayList<>();
+    int max = (int) Math.pow(2, n);
+    for (int i = 0; i < max; i++) {
+      List<Integer> result = new ArrayList<>();
+      for (int j = 0; j < n; j++) {
+        if ((i & (1 << j)) != 0)
+          result.add(nums[j]);
+      }
+      results.add(result);
+    }
+    return results;
+  }
+
   public static void main(String[] args) {
     try (Scanner sc = new Scanner(System.in)) {
-      String str = sc.next();
-      int n = str.length();
-      int powSize = (int) Math.pow(2,n);
-      for (int counter = 0; counter < powSize; counter++) {
-        for (int i = 0; i < n; i++) {
-          if ((counter&(1<<i)) != 0)
-            System.out.print(str.charAt(i));
-        }
-        System.out.println();
+      int N = sc.nextInt();
+      int[] arr = new int[N];
+      for (int i = 0; i < N; i++) {
+        arr[i] = sc.nextInt();
       }
+      System.out.println(subsets(arr));
+      System.out.println(subsetsWithDups(arr));
     }
   }
 }
