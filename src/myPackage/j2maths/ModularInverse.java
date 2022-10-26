971696b8d0c2a1003094058506958ffebe697c3e
Siddhant Vij <siddhantvij201@gmail.com>
2022-10-26 16:29:45 +0530

    Multiplicative Inverse under Modulus
    1. Fermat's Little Theorem
    2. Extended Euclidean Algorithm

--- src/myPackage/j2maths/ModularInverse.java
+++ src/myPackage/j2maths/ModularInverse.java
@@ -3,27 +3,56 @@ package myPackage.j2maths;
 import java.util.Scanner;
 
 public class ModularInverse {
-  public static int modularInverse(int x, int n){
+
+  private static boolean isPrime(int n) {
+    if (n == 1)
+      return false;
+    if (n == 2 || n == 3)
+      return true;
+    if (n % 2 == 0 || n % 3 == 0)
+      return false;
+    for (int i = 5; i * i <= n; i++) {
+      if (n % i == 0 || n % (i + 2) == 0)
+        return false;
+    }
+    return true;
+  }
+
+  private static int modularInverseNP(int a, int b, int t1, int t2) {
+    if (b == 0)
+      return t1;
+    return modularInverseNP(b, a % b, t2, t1 - t2 * (a / b));
+  }
+
+  public static int modularInverseP(int x, int n) {
     int result = 1;
     int num = n;
     n = n - 2;
-    while (n > 0)
-    {
-      if ((n & 1) == 1)
-      {
-        result = (result*x)%num;
+    while (n > 0) {
+      if ((n & 1) == 1) {
+        result = (result * x) % num;
       }
       n = n >> 1;
-      x = (x*x) % num;
+      x = (x * x) % num;
     }
     return result;
   }
-  
+
   public static void main(String[] args) {
     try (Scanner sc = new Scanner(System.in)) {
       int a = sc.nextInt();
       int m = sc.nextInt();
-      System.out.println(modularInverse(a,m));
+      int b = m;
+      int inverse = 0;
+      if (isPrime(m))
+        inverse = modularInverseP(a, m); // Fermat's Little Theorem
+      else {
+        if (a > m)
+          inverse = modularInverseNP(a, m, 0, 1); // Extended Euclidean Algorithm
+        else
+          inverse = modularInverseNP(m, a, 0, 1); // Extended Euclidean Algorithm
+      }
+      System.out.println(inverse < 0 ? b + inverse : inverse);
     }
   }
 }
