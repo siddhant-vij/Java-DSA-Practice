538ff1a63c7c849082f31346c0996d624d2d4e27
Siddhant Vij <siddhantvij201@gmail.com>
2022-11-07 12:12:52 +0530

    Bubble Sort Implementation Optimized

--- src/myPackage/j7Sorting/BubbleSort.java
+++ src/myPackage/j7Sorting/BubbleSort.java
@@ -40,7 +40,7 @@ public class BubbleSort {
   static void bubbleSortOptimized(int[] arr) {
     int n = arr.length;
     boolean swapped;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n-1; i++) {
       swapped = false;
       for (int j = 0; j < n - i - 1; j++) {
         if (arr[j] > arr[j + 1]) {
@@ -57,7 +57,7 @@ public class BubbleSort {
 
   static void bubbleSort(int[] arr) {
     int n = arr.length;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n-1; i++) {
       for (int j = 0; j < n - i - 1; j++) {
         if (arr[j] > arr[j + 1]) {
           int temp = arr[j];
