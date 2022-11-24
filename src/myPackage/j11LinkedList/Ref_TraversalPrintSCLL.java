e40d67117412c40f9ffe04666299a008c1e4c8cd
Siddhant Vij <siddhantvij201@gmail.com>
2022-11-24 15:22:00 +0530

    Reference for Traversal & Printing SCLL

---
+++ src/myPackage/j11LinkedList/Ref_TraversalPrintSCLL.java
@@ -0,0 +1,56 @@
+package myPackage.j11LinkedList;
+
+import java.util.Scanner;
+
+public class Ref_TraversalPrintSCLL {
+
+  static Node tail;
+
+  static class Node {
+    int val;
+    Node next;
+
+    Node(int val) {
+      this.val = val;
+    }
+  }
+
+  void printList() {
+    if (tail == null) {
+      System.out.println("null");
+      return;
+    }
+    if (tail.next == tail) {
+      System.out.println(tail.val + " --> HEAD");
+      return;
+    }
+    Node curNode = tail.next;
+    do {
+      System.out.print(curNode.val + " --> ");
+      curNode = curNode.next;
+    } while (curNode != tail);
+    System.out.println(curNode.val + " --> HEAD");
+  }
+
+  public static void main(String args[]) {
+    try (Scanner sc = new Scanner(System.in)) {
+      int n = sc.nextInt();
+      tail = null;
+      Node head = null;
+      if (n != 0) {
+        tail = new Node(sc.nextInt());
+        head = tail;
+      }
+      for (int i = 0; i < n - 1; i++) {
+        tail.next = new Node(sc.nextInt());
+        tail = tail.next;
+        if (i == n - 2)
+          tail.next = head;
+      }
+      if (n == 1)
+        tail.next = head;
+      Ref_TraversalPrintSCLL obj = new Ref_TraversalPrintSCLL();
+      obj.printList();
+    }
+  }
+}
