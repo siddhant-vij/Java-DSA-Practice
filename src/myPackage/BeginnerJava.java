c0b24eaceca66377cb401d31c92cd8075c1787f6
Siddhant Vij <siddhantvij201@gmail.com>
2022-12-02 10:39:53 +0000

    CodeSpace Test

--- src/myPackage/BeginnerJava.java
+++ src/myPackage/BeginnerJava.java
@@ -6,7 +6,6 @@ public class BeginnerJava {
   public static void main(String[] args) {
     try (Scanner sc = new Scanner(System.in)) {
       int x = sc.nextInt();
-      // x = 3000
       System.out.println("Hello World " + x);
     }
   }
