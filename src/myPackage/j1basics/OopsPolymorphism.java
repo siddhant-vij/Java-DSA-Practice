7adb35cd369b1b98564662c5e6212dfe52ee697d
Siddhant Vij <siddhantvij201@gmail.com>
2022-12-17 16:10:42 +0530

    Polymorphism Example in Java

--- src/myPackage/j1basics/OopsPolymorphism.java
+++ src/myPackage/j1basics/OopsPolymorphism.java
@@ -49,6 +49,63 @@ class GasPoweredCar extends Car {
   }
 }
 
+class ElectricCar extends Car {
+
+  private double avgKmPerCharge;
+  private int batterySize = 6;
+
+  public ElectricCar(String description) {
+    super(description);
+  }
+
+  public ElectricCar(String description, double avgKmPerCharge, int batterySize) {
+    super(description);
+    this.avgKmPerCharge = avgKmPerCharge;
+    this.batterySize = batterySize;
+  }
+
+  @Override
+  public void startEngine() {
+    System.out.printf("BEV -> switch %d kWh battery on, Ready!%n", batterySize);
+  }
+
+  @Override
+  protected void runEngine() {
+    System.out.printf("BEV -> usage under the average: %.2f %n", avgKmPerCharge);
+  }
+}
+
+class HybridCar extends Car {
+
+  private double avgKmPerLiter;
+  private int cylinders = 6;
+
+  private int batterySize;
+
+  public HybridCar(String description) {
+    super(description);
+  }
+
+  public HybridCar(String description, double avgKmPerLiter, int cylinders,
+      int batterySize) {
+    super(description);
+    this.avgKmPerLiter = avgKmPerLiter;
+    this.cylinders = cylinders;
+    this.batterySize = batterySize;
+  }
+
+  @Override
+  public void startEngine() {
+    System.out.printf("Hybrid -> %d cylinders are fired up.%n", cylinders);
+    System.out.printf("Hybrid -> switch %d kWh battery on, Ready!%n", batterySize);
+  }
+
+  @Override
+  protected void runEngine() {
+    System.out.printf("Hybrid -> usage below average: %.2f %n", avgKmPerLiter);
+  }
+}
+
 public class OopsPolymorphism {
 
   public static void runRace(Car car) {
@@ -57,10 +114,19 @@ public class OopsPolymorphism {
   }
 
   public static void main(String[] args) {
-    Car car = new Car("2018 Maruti Alto 273 OTR");
+    Car car = new Car("2022 Blue Ferrari 296 GTS");
     runRace(car);
 
-    Car ferrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS", 15.4, 6);
+    Car ferrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS",
+        15.4, 6);
     runRace(ferrari);
+
+    Car tesla = new ElectricCar("2022 Red Tesla Model 3",
+        568, 75);
+    runRace(tesla);
+
+    Car ferrariHybrid = new HybridCar("2022 Black Ferrari SF90 Stradale",
+        16, 8, 8);
+    runRace(ferrariHybrid);
   }
 }
