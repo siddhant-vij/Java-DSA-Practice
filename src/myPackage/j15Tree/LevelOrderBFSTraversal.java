ceecc7d020d8038854a32a0b2f5b015d21819685
Siddhant Vij <siddhantvij201@gmail.com>
2023-01-03 14:13:30 +0530

    Practice: Level Order BFS Traversal

---
+++ src/myPackage/j15Tree/LevelOrderBFSTraversal.java
@@ -0,0 +1,74 @@
+package myPackage.j15Tree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class LevelOrderBFSTraversal {
+
+  static class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node(int val) {
+      this.val = val;
+    }
+  }
+
+  static Node buildTree(String str) {
+    if (str.length() == 0 || str.charAt(0) == 'N') {
+      return null;
+    }
+    String ip[] = str.split(" ");
+    Node root = new Node(Integer.parseInt(ip[0]));
+    Queue<Node> queue = new ArrayDeque<>();
+    queue.add(root);
+    int i = 1;
+    while (queue.size() > 0 && i < ip.length) {
+      Node currNode = queue.peek();
+      queue.remove();
+      String currVal = ip[i];
+      if (!currVal.equals("N")) {
+        currNode.left = new Node(Integer.parseInt(currVal));
+        queue.add(currNode.left);
+      }
+      i++;
+      if (i >= ip.length)
+        break;
+      currVal = ip[i];
+      if (!currVal.equals("N")) {
+        currNode.right = new Node(Integer.parseInt(currVal));
+        queue.add(currNode.right);
+      }
+      i++;
+    }
+    return root;
+  }
+
+  static List<Integer> levelOrderTraversal(Node root, List<Integer> result) {
+    if (root == null)
+      return result;
+    Queue<Node> queue = new ArrayDeque<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      Node curNode = queue.remove();
+      result.add(curNode.val);
+      if (curNode.left != null)
+        queue.add(curNode.left);
+      if (curNode.right != null)
+        queue.add(curNode.right);
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      String str = sc.nextLine();
+      Node root = buildTree(str);
+      System.out.println(levelOrderTraversal(root, new ArrayList<>()));
+    }
+  }
+}
