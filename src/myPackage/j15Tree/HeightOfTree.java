841ee3d4dc478d21021f027a5a2f6cf0724612b6
Siddhant Vij <siddhantvij201@gmail.com>
2023-01-03 13:23:12 +0530

    Practice: Height Of Tree

---
+++ src/myPackage/j15Tree/HeightOfTree.java
@@ -0,0 +1,63 @@
+package myPackage.j15Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class HeightOfTree {
+
+  static class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node(int val) {
+      this.val = val;
+    }
+  }
+
+  static Node buildTree(String str) {
+    if (str.length() == 0 || str.charAt(0) == 'N') {
+      return null;
+    }
+    String ip[] = str.split(" ");
+    Node root = new Node(Integer.parseInt(ip[0]));
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(root);
+    int i = 1;
+    while (queue.size() > 0 && i < ip.length) {
+      Node currNode = queue.peek();
+      queue.remove();
+      String currVal = ip[i];
+      if (!currVal.equals("N")) {
+        currNode.left = new Node(Integer.parseInt(currVal));
+        queue.add(currNode.left);
+      }
+      i++;
+      if (i >= ip.length)
+        break;
+      currVal = ip[i];
+      if (!currVal.equals("N")) {
+        currNode.right = new Node(Integer.parseInt(currVal));
+        queue.add(currNode.right);
+      }
+      i++;
+    }
+    return root;
+  }
+
+  static int height(Node root) {
+    if(root == null) {
+      return 0;
+    }
+    return 1 + Math.max(height(root.left), height(root.right));
+  }
+
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      String str = sc.nextLine();
+      Node root = buildTree(str);      
+      System.out.println(height(root));
+    }
+  }  
+}
